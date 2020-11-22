d7cc43ed0e0a75e7a1e7a74170d05b7c4b6ebd15
0713Eric3742 <u10831016@lssh.tp.edu.tw>
2020-11-22 16:37:36 +0800

    first commit

--- Cpp/database/n.cpp
+++ Cpp/database/n.cpp
@@ -1,44 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-int f(int n)
-{
-    long long fib[200005];
-    fib[1] = 1;
-    fib[2] = 1;
-    for(int i=3 ; i<=n ; i=i+1 ) fib[i] = (fib[i-1] + fib[i-2])%998244353;
-	return fib[n];
-}
-int power(int x, int y, int p) 
-{
-	int res = 1;
-	x = x % p;
-	while (y > 0) 
-	{ 
-		if (y & 1) res = (res*x) % p; 
-		y = y>>1;
-		x = (x*x) % p; 
-	} 
-	return res;
-}
-long long calculate(long long p,long long q) 
-{ 
-    long long mod = 998244353, expo; 
-    expo = mod - 2; 
-    while (expo)
-	{ 
-        if (expo & 1)
-		{ 
-            p = (p * q) % mod; 
-        } 
-        q = (q * q) % mod; 
-        expo >>= 1; 
-    } 
-    return p; 
-} 
+int c[100005]={};
 int main()
 {
-	int a;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int a,b,d,e;
+	vector<int> ans;
 	cin>>a;
-	cout<<(f(a)*calculate(1,power(2,a,998244353)))%998244353;
+	for (int i = 0; i < a; i++)
+	{
+		cin>>c[i];
+	}
+	cin>>b;
+	for (int i = 0; i < b; i++)
+	{
+		cin>>d>>e;
+		ans.clear();
+		for (int j = d; j <= e; j++)
+		{
+			if(j%2==0)
+			if(j%2==0)
+		
+	}
+	
 	return 0;
 }
--- Cpp/database/n1.cpp
+++ Cpp/database/n1.cpp
@@ -2,9 +2,23 @@
 using namespace std;
 int main()
 {
-	double a;
+	long long int a,b[200005],c[200005],ans=0;
 	cin>>a;
-	cout<<0.131868*6.5<<endl;
-	//cout<< (TP*6.5 - TS*3.5) / (6.5 - 3.5)<<endl;
+	for (int i = 0; i < a; i++)
+	{
+		cin>>b[i];
+	}
+	sort(b,b+a);
+	reverse(b,b+a);
+	for (int i = 0; i < a; i++)
+	{
+		if(i==0) c[i]=b[i];
+		else c[i]=c[i-1]+b[i];
+	}
+	for (int i = 1; i < a; i++)
+	{
+		ans+=c[i-1]-b[i]*i;
+	}
+	cout<<ans<<endl;
 	return 0;
 }

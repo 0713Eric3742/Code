373aad0040f5e6b501e609058523d78fec562cae
0713Eric3742 <u10831016@lssh.tp.edu.tw>
2020-11-29 17:07:37 +0800

    first commit

--- Cpp/database/Bit.cpp
+++ Cpp/database/Bit.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int BIT[200005];
 int lowbit(int val)
@@ -7,55 +7,55 @@ int lowbit(int val)
 }
 int main(int argc, char const *argv[])
 {
-	int a,b,c[200005],t,ans,v;
-	cin>>a;
+	int a, b, c[200005], t, ans, v;
+	cin >> a;
 	for (int i = 1; i <= a; i++)
 	{
-		cin>>c[i];
+		cin >> c[i];
 	}
 	//---- create BIT ------
 	for (int i = 0; i <= a; i++)
 	{
-		BIT[i]=0;
+		BIT[i] = 0;
 	}
 	for (int i = 0; i <= a; i++)
 	{
-		t=i-1;
-		BIT[i]=c[i];
-		while(t>i-lowbit(i))
+		t = i - 1;
+		BIT[i] = c[i];
+		while (t > i - lowbit(i))
 		{
-			BIT[i]+=BIT[t];
-			t-=lowbit(t);
+			BIT[i] += BIT[t];
+			t -= lowbit(t);
 		}
 	}
 	for (int i = 1; i <= a; i++)
 	{
-		cout<<BIT[i]<<' ';
+		cout << BIT[i] << ' ';
 	}
-	cout<<endl;
+	cout << endl;
 	//-----------------------
 	//----- change val ------
-	cin>>b>>v;
-	while(b<=a)
+	cin >> b >> v;
+	while (b <= a)
 	{
-		BIT[b]+=v;
-		b+=lowbit(b);
+		BIT[b] += v;
+		b += lowbit(b);
 	}
 	for (int i = 1; i <= a; i++)
 	{
-		cout<<BIT[i]<<' ';
+		cout << BIT[i] << ' ';
 	}
-	cout<<endl;
+	cout << endl;
 	//-------------------------
 	//--------- query ---------
-	cin>>b;
-	ans=0;
-	while(b>=1)
+	cin >> b;
+	ans = 0;
+	while (b >= 1)
 	{
-		ans+=BIT[b];
-		b-=lowbit(b);
+		ans += BIT[b];
+		b -= lowbit(b);
 	}
-	cout<<ans<<endl;
+	cout << ans << endl;
 	//--------------------------
 	return 0;
 }
